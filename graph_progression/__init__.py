502a496e6b3c47b65dfad3f46e21495edcce33c4
Marco Gancitano <marco.gancitano97@gmail.com>
2022-02-07 16:34:25 -0500

    Add metadata to package base

--- graph_progression/__init__.py
+++ graph_progression/__init__.py
@@ -1,6 +1,6 @@
 """Progression namespace."""
 
-# from importlib_metadata import PackageNotFoundError, version
+from importlib_metadata import PackageNotFoundError, version
 
 from .generators import random_walk
 from .progressors import DFSProgressor
@@ -11,11 +11,15 @@ __email__ = "marco.gancitano@askarthur.art"
 
 # Used to automatically set version number from github actions
 # as well as not break when being tested locally
-"""
 try:
     __version__ = version(__package__)  # type: ignore
 except PackageNotFoundError:  # pragma: no cover
     __version__ = "0.0.0"
-"""
 
-__all__ = ["DFSProgressor", "first", "last", "randomness", "random_walk"]
+__all__ = [
+    "DFSProgressor",
+    "first",
+    "last",
+    "randomness",
+    "random_walk",
+]
